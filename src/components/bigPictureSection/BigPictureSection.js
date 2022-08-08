50cf210623328819a83d744f9fc9da8fff1ee83d
MarzioCostantini <costantinimarzio@gmail.com>
2022-08-08 14:30:33 +0200

    Unterseiten Bug Fixing

--- src/components/bigPictureSection/BigPictureSection.js
+++ src/components/bigPictureSection/BigPictureSection.js
@@ -1,17 +1,20 @@
 const BigPictureSection = () => {
-    return (
-        <section className="bigPictureSection">
-            <div className="wrap">
-                <div className="textWrap">
-                    <h2>Bist du bereit</h2>
-                    <h2>durchzustarten?</h2>
-                    <a className="btn" href="#allCourses">
-                        Kursübersicht
-                    </a>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="bigPictureSection">
+      <div className="wrap">
+        <div className="textWrap">
+          <div>
+            <h2>Bist du bereit</h2>
+            <h2>durchzustarten?</h2>
+          </div>
+
+          <a className="btn" href="#allCourses">
+            Kursübersicht
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BigPictureSection;
