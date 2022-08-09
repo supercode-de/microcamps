bdb1f7d1b2925cd3a48c3065595f6be87f4d7bb3
MarzioCostantini <costantinimarzio@gmail.com>
2022-08-09 15:58:19 +0200

    Filter Error Hinzugefügt bei kein Kurs

--- src/components/allCourses_neu/AllCoursesListNeu.js
+++ src/components/allCourses_neu/AllCoursesListNeu.js
@@ -1,138 +1,130 @@
 import { useEffect, useState } from "react";
 import AllCoursesCardNeu from "./AllCoursesCardNeu";
+import FehlermeldungSection from "../fehlermeldungSection/FehlermeldungSection";
 
 import courseData from "../../data/courseData.json";
 
 const AllCoursesListNeu = (props) => {
-    const [filteredArr, setFilteredArr] = useState(courseData);
+  const [filteredArr, setFilteredArr] = useState(courseData);
 
-    const [filterWhat, setFilterWhat] = useState([]);
-    const insertfilterWhatState = (status) => {
-        if (filterWhat.includes(status)) {
-            setFilterWhat(filterWhat.filter((item) => item !== status));
-        } else {
-            setFilterWhat([...filterWhat, status]);
-        }
-    };
-    const [filterFor, setFilterFor] = useState([]);
-    const insertfilterForState = (status) => {
-        if (filterFor.includes(status)) {
-            setFilterFor(filterFor.filter((item) => item !== status));
-        } else {
-            setFilterFor([...filterFor, status]);
-        }
-    };
-    useEffect(() => {
-        const filteredFor = courseData.filter((item) => {
-            if (filterFor.length <= 0) {
-                return item;
-            } else {
-                return (
-                    item.level === filterFor[0] || item.level === filterFor[1]
-                );
-            }
-        });
-        const filteredAll = filteredFor.filter((item) => {
-            if (filterWhat.length <= 0) {
-                return item;
-            } else {
-                return (
-                    item.theme === filterWhat[0] ||
-                    item.theme === filterWhat[1] ||
-                    item.theme === filterWhat[2]
-                );
-            }
-        });
+  const [filterWhat, setFilterWhat] = useState([]);
+  const insertfilterWhatState = (status) => {
+    if (filterWhat.includes(status)) {
+      setFilterWhat(filterWhat.filter((item) => item !== status));
+    } else {
+      setFilterWhat([...filterWhat, status]);
+    }
+  };
+  const [filterFor, setFilterFor] = useState([]);
+  const insertfilterForState = (status) => {
+    if (filterFor.includes(status)) {
+      setFilterFor(filterFor.filter((item) => item !== status));
+    } else {
+      setFilterFor([...filterFor, status]);
+    }
+  };
+  useEffect(() => {
+    const filteredFor = courseData.filter((item) => {
+      if (filterFor.length <= 0) {
+        return item;
+      } else {
+        return item.level === filterFor[0] || item.level === filterFor[1];
+      }
+    });
+    const filteredAll = filteredFor.filter((item) => {
+      if (filterWhat.length <= 0) {
+        return item;
+      } else {
+        return (
+          item.theme === filterWhat[0] ||
+          item.theme === filterWhat[1] ||
+          item.theme === filterWhat[2]
+        );
+      }
+    });
 
-        setFilteredArr(filteredAll);
-    }, [filterWhat, filterFor]);
-    return (
-        <article className="allCoursesArea__list">
-            <div className="allCoursesArea__list__filter">
-                <p>Filter</p>
-                <input
-                    className="hidden"
-                    onChange={(e) => insertfilterWhatState(e.target.value)}
-                    type="checkbox"
-                    name="filter-what"
-                    id="coding-button"
-                    value="coding"
-                />
-                <label
-                    className="button-label coding-label"
-                    htmlFor="coding-button"
-                >
-                    <p>coding</p>
-                </label>
-                <input
-                    className="hidden"
-                    onChange={(e) => insertfilterWhatState(e.target.value)}
-                    type="checkbox"
-                    name="filter-what"
-                    id="design-button"
-                    value="design"
-                />
-                <label
-                    className="button-label design-label"
-                    htmlFor="design-button"
-                >
-                    <p>design</p>
-                </label>
-                <input
-                    className="hidden"
-                    onChange={(e) => insertfilterWhatState(e.target.value)}
-                    type="checkbox"
-                    name="filter-what"
-                    id="data-button"
-                    value="data"
-                />
-                <label
-                    className="button-label data-label"
-                    htmlFor="data-button"
-                >
-                    <p>data</p>
-                </label>
-                {/* ---------------------- */}
-                <input
-                    className="hidden"
-                    onChange={(e) => insertfilterForState(e.target.value)}
-                    type="checkbox"
-                    name="filter-for"
-                    id="expert-button"
-                    value="expert"
-                />
-                <label
-                    className="button-label expert-label"
-                    htmlFor="expert-button"
-                >
-                    <p>expert</p>
-                </label>
-                <input
-                    className="hidden"
-                    onChange={(e) => insertfilterForState(e.target.value)}
-                    type="checkbox"
-                    name="filter-for"
-                    id="beginner-button"
-                    value="Beginner"
-                />
-                <label
-                    className="button-label beginner-label"
-                    htmlFor="beginner-button"
-                >
-                    <p>beginner</p>
-                </label>
-            </div>
-            <div className="allCoursesArea__list__viewList">
-                {filteredArr.map((course) => (
-                    <AllCoursesCardNeu
-                        key={course.id}
-                        data={course}
-                        filteredTools={props.filteredTools}
-                    />
-                ))}
-            </div>
-        </article>
-    );
+    setFilteredArr(filteredAll);
+  }, [filterWhat, filterFor]);
+  return (
+    <article className="allCoursesArea__list">
+      <div className="allCoursesArea__list__filter">
+        <p>Filter</p>
+        <input
+          className="hidden"
+          onChange={(e) => insertfilterWhatState(e.target.value)}
+          type="checkbox"
+          name="filter-what"
+          id="coding-button"
+          value="coding"
+        />
+        <label className="button-label coding-label" htmlFor="coding-button">
+          <p>coding</p>
+        </label>
+        <input
+          className="hidden"
+          onChange={(e) => insertfilterWhatState(e.target.value)}
+          type="checkbox"
+          name="filter-what"
+          id="design-button"
+          value="design"
+        />
+        <label className="button-label design-label" htmlFor="design-button">
+          <p>design</p>
+        </label>
+        <input
+          className="hidden"
+          onChange={(e) => insertfilterWhatState(e.target.value)}
+          type="checkbox"
+          name="filter-what"
+          id="data-button"
+          value="data"
+        />
+        <label className="button-label data-label" htmlFor="data-button">
+          <p>data</p>
+        </label>
+        {/* ---------------------- */}
+        <input
+          className="hidden"
+          onChange={(e) => insertfilterForState(e.target.value)}
+          type="checkbox"
+          name="filter-for"
+          id="expert-button"
+          value="expert"
+        />
+        <label className="button-label expert-label" htmlFor="expert-button">
+          <p>expert</p>
+        </label>
+        <input
+          className="hidden"
+          onChange={(e) => insertfilterForState(e.target.value)}
+          type="checkbox"
+          name="filter-for"
+          id="beginner-button"
+          value="Beginner"
+        />
+        <label
+          className="button-label beginner-label"
+          htmlFor="beginner-button"
+        >
+          <p>beginner</p>
+        </label>
+      </div>
+
+      {filteredArr.length === 0 ? (
+        <FehlermeldungSection />
+      ) : (
+        <div className="allCoursesArea__list__viewList">
+          {filteredArr.map((course) => (
+            <AllCoursesCardNeu
+              key={course.id}
+              data={course}
+              filteredTools={props.filteredTools}
+            />
+          ))}
+        </div>
+      )}
+    </article>
+  );
 };
 
 export default AllCoursesListNeu;
