1e7acfa2b9b5a70af6e62e37397296af0d882cc1
MarzioCostantini <costantinimarzio@gmail.com>
2022-08-09 12:37:57 +0200

    Fehlermeldung bei Kursliste

---
+++ src/components/fehlermeldungSection/FehlermeldungSection.js
@@ -0,0 +1,11 @@
+import "./FehlermeldungSection.scss";
+
+const FehlermeldungSection = () => {
+  return (
+    <div className="fehlermeldung-courses">
+      <h3>Dazu haben wir leider noch keine Micro Classes im Angebot. :(</h3>
+    </div>
+  );
+};
+
+export default FehlermeldungSection;
--- src/components/ourStorySection/OurStorySection.js
+++ src/components/ourStorySection/OurStorySection.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const OurStorySection = () => {
     return (
         <section className="ourStorySection">
@@ -41,7 +43,10 @@ const OurStorySection = () => {
                             </p>
                         </div>
                         <div className="image">
+                            <Link to="/supercamp/our-story">
                             <p>unsere Geschichte</p>
+                            </Link>
+                           
                             <img
                                 src="img/sergio02.png"
                                 alt="Sergio, Founder of Supercamp"
