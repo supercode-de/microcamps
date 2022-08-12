33ecd11f2c6617b7f16847d4a22fc06e88794ab9
MarzioCostantini <costantinimarzio@gmail.com>
2022-08-12 14:02:55 +0200

    FAQ Bugfix

--- src/components/faqSection/FaqSection.js
+++ src/components/faqSection/FaqSection.js
@@ -1,25 +1,34 @@
 import FaqSectionAccCard from "./FaqSectionAccCard";
 import faqdata from "../../data/faqdata";
-const FaqSection = () => {
-    return (
-        <section className="faqSection">
-            <div className="wrap" id="faq">
-                <h2>
-                    <span className="headlines_stroke">FAQ</span>
-                </h2>
-                <div className="faqSection__accordion">
-                    {faqdata.map((faqItem, index) => (
-                        <FaqSectionAccCard
-                            key={index}
-                            frage={faqItem.frage}
-                            antwort={faqItem.antwort}
-                        />
 
-                    ))}
-                </div>
-            </div>
-        </section>
-    );
+import { useState } from "react";
+
+const FaqSection = () => {
+  const [expanded, setExpanded] = useState("");
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+  return (
+    <section className="faqSection">
+      <div className="wrap" id="faq">
+        <h2>
+          <span className="headlines_stroke">FAQ</span>
+        </h2>
+        <div className="faqSection__accordion">
+          {faqdata.map((faqItem, index) => (
+            <FaqSectionAccCard
+              expanded={expanded}
+              handleChange={handleChange}
+              key={index}
+              frage={faqItem.frage}
+              antwort={faqItem.antwort}
+              index={index}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FaqSection;
