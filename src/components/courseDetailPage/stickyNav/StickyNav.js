371e3ea36203e94294b388fbbc35e2bd53fcfd53
Finnschy <schlottmannfinn@gmail.com>
2022-08-12 16:55:29 +0200

    ungenutzte hashlink importe gelöscht weil netlify gemeckert hat

--- src/components/courseDetailPage/stickyNav/StickyNav.js
+++ src/components/courseDetailPage/stickyNav/StickyNav.js
@@ -1,4 +1,3 @@
-import { NavHashLink } from "react-router-hash-link";
 import { Link } from "react-scroll";
 
 const StickyNav = () => {
@@ -9,27 +8,22 @@ const StickyNav = () => {
                 <ul>
                     <li>
                         <Link activeClass="active" smooth spy to="overviewSection">Übersicht</Link>
-                        {/* <NavHashLink to="#overviewSection">Übersicht</NavHashLink> */}
                     </li>
                     <li>
                         <Link activeClass="active" smooth spy to="curriculumSection">Curriculum</Link>
-                        {/* <NavHashLink to="#curriculumSection">Curriculum</NavHashLink> */}
                     </li>
                     <li>
                         <Link activeClass="active" smooth spy to="scheduleSection">Zeitplan</Link>
-                        {/* <NavHashLink to="#scheduleSection">Zeitplan</NavHashLink> */}
                     </li>
                     <li>
                         <Link activeClass="active" smooth spy to="teamSection">Team</Link>
                     </li>
                     <li>
                         <Link activeClass="active" smooth spy to="toolsSection">Tools</Link>
-
                     </li>
                     <li>
                         <Link activeClass="active" smooth spy to="nextBootcamp">Termine</Link>
                     </li>
-
                 </ul>
             </div>
         </section>
